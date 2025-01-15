d07a76c6eb3eb37e2fd17a7acc350c74f242d4b6
Wael Mejdoub <wael.mejdoub.dev@gmail.com>
2025-01-15 12:38:56 +0100

    starter

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+import { PanelsTopLeft } from "lucide-react";
+import { ArrowRightIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="min-h-[calc(100vh-57px-97px)] flex-1">
+        <div className="container relative pb-10">
+          <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-6">
+            <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+              Homepahe
+            </h1>
+            <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+              <Button variant="default" asChild>
+                <Link href="/dashboard">
+                  Demo
+                  <ArrowRightIcon className="ml-2" />
+                </Link>
+              </Button>
+              <Button variant="outline" asChild>
+                <Link
+                  href="/login"
+                >
+                  Login
+                </Link>
+              </Button>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
